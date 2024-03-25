b522268464bc270e349c4fbc6f65a7370a1227f2
Efe Yanar <efe1yanar@gmail.com>
2024-03-25 13:36:34 +0300

    Add files via upload

---
+++ While!=0/While!=0/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Sistemi Kapatmak İçin '0' Giriniz.");
+        int i, toplam, sayi;
+        i = 1;
+
+        Console.Write("1. Sayıyı Giriniz  = ");
+        sayi = (int)Convert.ToInt64(Console.ReadLine());
+        toplam = sayi;
+
+        while (sayi != 0)
+        {
+            i++;
+            Console.Write(i + ". Sayıyı Giriniz = ");
+            sayi = (int)Convert.ToInt64(Console.ReadLine());
+            toplam = toplam + sayi;
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine(i + " Sayı Girişi Yaptınız,");
+        Console.WriteLine("Girdiğiniz Sayıların Toplamı = " + toplam);
+
+        Console.ReadKey();
+    }
+}
